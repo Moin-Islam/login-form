5d4ada988c8f946d8607620e1d538755866220b7
Moin-Islam <pkarnival@gmail.com>
2023-10-09 13:07:54 +0600

    4th commit

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ function Validation () {
     let password = userPassword.value;
 
     if(name.length < 6) {
-        console.log("username must be 6 character");
         document.querySelector(".usernameError").style.display = "block";
         setTimeout(() => {
             document.querySelector(".usernameError").style.display = "none";
           }, 3000)
+    }
+    else {
+        console.log(name);
     }
     if(email.includes('@')) {
         console.log (email);
@@ -23,14 +25,12 @@ function Validation () {
           }, 3000)
     }
     if (name == password) {
-        console.log("username cann't be the same as password");
         document.querySelector(".passwordError").style.display = "block";
         setTimeout(() => {
             document.querySelector(".passwordError").style.display = "none";
           }, 3000)
     } else if (password.length <6)
     {
-        console.log("Password must be 6 character long");
         document.querySelector(".passwordError1").style.display = "block";
         setTimeout(() => {
             document.querySelector(".passwordError1").style.display = "none";
