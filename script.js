4d92714fe3c157bbe507b69fd59cacff3cd5d3ba
Moin-Islam <pkarnival@gmail.com>
2023-10-10 15:27:01 +0600

    5th commit

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+/*function getFileSizeInKB(file) {
+    return file.size / 1024;
+}*/
+
+function updateImage() {
+  let profilePic = document.getElementById("profile-pic");
+  let inputFile = document.getElementById("input-file");
+
+  let fileSizeInKB;
+
+  inputFile.onchange = function () {
+    if (inputFile.files.length > 0)
+    {
+        profilePic.src = URL.createObjectURL(inputFile.files[0]);
+        fileSizeInKB = inputFile.files[0].size /1024;
+        
+        if(fileSizeInKB >50)
+        {
+            profilePic.src = "images/profile.png";
+            document.querySelector(".imageSizeError").style.display = "block";
+            console.log(fileSizeInKB);
+        }
+        else{
+            document.querySelector(".imageSizeError").style.display = "none";
+            console.log(fileSizeInKB);
+        }
+    }else {
+        alert("Please upload an image");
+    }
+  };
+  console.log(fileSizeInKB);
+}
+
+
 function nameValidation() {
   let userName = document.querySelector("#name").value;
 
@@ -29,29 +63,29 @@ function passwordValidation() {
     document.querySelector(".passwordError2").style.display = "block";
   } else if (userPassword.length < 6) {
     document.querySelector(".passwordError1").style.display = "block";
-  } else if (userPassword == name)
-  {
+    document.querySelector(".passwordError2").style.display = "none";
+    document.querySelector(".passwordError").style.display = "none";
+  } else if (userPassword == name) {
     document.querySelector(".passwordError").style.display = "block";
+    document.querySelector(".passwordError1").style.display = "none";
+    document.querySelector(".passwordError2").style.display = "none";
   } else {
     console.log(userPassword);
     document.querySelector(".passwordError").style.display = "none";
     document.querySelector(".passwordError1").style.display = "none";
     document.querySelector(".passwordError2").style.display = "none";
   }
-  
 }
 
 function confirmPassValidation() {
-    let password = document.querySelector("#pass").value;
-    let confirmPassword = document.querySelector("#confirm-pass").value;
+  let password = document.querySelector("#pass").value;
+  let confirmPassword = document.querySelector("#confirm-pass").value;
 
-    if (confirmPassword != password) {
-        document.querySelector(".confirmPasswordError").style.display = "block";
-    }else 
-    {
-        document.querySelector(".confirmPasswordError").style.display = "none";
-        console.log(confirmPassword);
-        console.log(password);
-    }
-
+  if (confirmPassword != password) {
+    document.querySelector(".confirmPasswordError").style.display = "block";
+  } else {
+    document.querySelector(".confirmPasswordError").style.display = "none";
+    console.log(confirmPassword);
+    console.log(password);
+  }
 }
